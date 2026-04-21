1f05698e037e4abcdaaacf137f98623c66a54d9c
changqi <changqi@vt.edu>
2026-04-21 00:09:48 +0000

    feat: add onion ring homomorphic operations

---
+++ include/oram/onion_ring/HomOps.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "tfhe.h"
+
+namespace oram::onion_ring {
+
+void ExternalProduct(TLweSample* result, const TGswSample* rgsw, const TLweSample* rlwe,
+                     const TGswParams* params);
+
+void CMux(TLweSample* result, const TGswSample* control, const TLweSample* d1,
+          const TLweSample* d0, const TGswParams* params);
+
+void Subs(TLweSample* result, const TLweSample* input, int32_t ai, const TLweParams* params);
+
+}  // namespace oram::onion_ring
